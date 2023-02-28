c1f8385165549c90401e9a4256957a7442f52db9
tonyliu <a0921444212@gmail.com>
2023-02-28 15:43:10 +0800

    Build q_insert_tail()
    Implementation just like q_insert_head, but in last part "list_add()" we change head node to head->prev

--- queue.c
+++ queue.c
@@ -68,6 +68,21 @@ bool q_insert_head(struct list_head *head, char *s)
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+    element_t *node = (element_t *) malloc(sizeof(element_t));
+    if (!node)
+        return false;
+    if (!node->value) {
+        free(node);
+        node = NULL;
+        return false;
+    }
+    node->value = (char *) malloc(sizeof(char) * (strlen(s) + 1));
+    memcpy(node->value, s, strlen(s));
+    node->value[strlen(s)] = '\0';
+    list_add(&node->list, head->prev);
+
     return true;
 }
 
